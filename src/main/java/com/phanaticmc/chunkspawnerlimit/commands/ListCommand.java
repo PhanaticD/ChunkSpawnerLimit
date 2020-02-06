dde8ff93441a096c01c367792a646da56ffe93dc
PhanaticD <javw.phanatic@gmail.com>
2020-02-06 02:32:04 -0500

    fixes

--- src/main/java/com/phanaticmc/chunkspawnerlimit/commands/ListCommand.java
+++ src/main/java/com/phanaticmc/chunkspawnerlimit/commands/ListCommand.java
@@ -13,22 +13,19 @@ import org.bukkit.command.CommandSender;
 public class ListCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("spawnerlist")) {
-			Bukkit.getWorlds().stream().forEach((w) -> {
-				for (Chunk chunk : w.getLoadedChunks()) {
-					int spawnercount = 0;
-					for (BlockState block : chunk.getTileEntities()) {
-						if (block instanceof CreatureSpawner) {
-							spawnercount++;
-							if (spawnercount > limit) {
-								sender.sendMessage("Over Limit Spawner: " + block.getLocation().getBlockX() + " " + block.getLocation().getBlockY() + " " + block.getLocation().getBlockZ() + " " + block.getLocation().getWorld().getName());
-							}
+		Bukkit.getWorlds().forEach((w) -> {
+			for (Chunk chunk : w.getLoadedChunks()) {
+				int spawnercount = 0;
+				for (BlockState block : chunk.getTileEntities()) {
+					if (block instanceof CreatureSpawner) {
+						spawnercount++;
+						if (spawnercount > limit) {
+							sender.sendMessage("Over Limit Spawner: " + block.getLocation().getBlockX() + " " + block.getLocation().getBlockY() + " " + block.getLocation().getBlockZ() + " " + block.getLocation().getWorld().getName());
 						}
 					}
 				}
-			});
-			return true;
-		}
-		return false;
+			}
+		});
+		return true;
 	}
 }
